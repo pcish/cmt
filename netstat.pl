19e2e2b96aa65b0cc7160d11b4e6120e935935b1
Paul Chiang <paul_chiang@tcloudcomputing.com>
2010-06-22 15:06:29 +0800

    netstat:

--- netstat.pl
+++ netstat.pl
@@ -4,6 +4,16 @@ if (!defined($ARGV[0])) {
     print "Must supply interface argument.\n";
     exit;
 }
+if ($ARGV[0] == '-l') {
+    my $lxs = Sys::Statistics::Linux::NetStats->new;
+    $lxs->init;
+    $stat = $lxs->get;
+    $iface_stat = %$stat->{'eth0'};
+    foreach $k (keys(%$iface_stat)) {
+        print $k."\n";
+    }
+    exit;
+}
 my $iface = $ARGV[0];
 my $lxs = Sys::Statistics::Linux::NetStats->new;
 $lxs->init;
@@ -16,8 +26,9 @@ while(1) {
     $iface_stat = %$stat->{$iface};
     while (($k, $v) = each %$iface_stat) {
         if ($k eq "rxbyt" || $k eq "rxpcks" ||
+            $k eq "txdrop" || $k eq "rxdrop" ||
             $k eq "txbyt" || $k eq "txpcks" ) {
-            print $k.".".$v."\t";
+            print $k."=".$v."\t";
         }
     }
     print "\n";
